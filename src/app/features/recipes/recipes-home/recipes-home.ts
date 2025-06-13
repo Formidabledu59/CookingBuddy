19fff572bb3b54bf75b94b6bfbf34006d709dff5
Ayoub Fathallah <130578160+Formidabledu59@users.noreply.github.com>
2025-06-13 21:32:55 +0200

    feat: recherche feature

--- src/app/features/recipes/recipes-home/recipes-home.ts
+++ src/app/features/recipes/recipes-home/recipes-home.ts
@@ -2,8 +2,12 @@ import { Component, inject, signal, computed } from '@angular/core';
 import { RecipesService } from '../../../core/services/recipes';
 import { RecipeCategories } from '../recipe-categories/recipe-categories';
 import { RecipeCategory } from '../recipe-category/recipe-category';
+import { DetailedRecipeCard } from '../detailed-recipe-card/detailed-recipe-card';
 import { PageLayoutComponent } from '../../../shared/layouts/page-layout/page-layout';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipes-home',
@@ -12,7 +16,11 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     PageLayoutComponent,
     RecipeCategories,
-    RecipeCategory
+    RecipeCategory,
+    DetailedRecipeCard,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   templateUrl: './recipes-home.html',
   styleUrl: './recipes-home.scss',
@@ -20,6 +28,7 @@ import { CommonModule } from '@angular/common';
 export class RecipesHome {
   private readonly recipesService = inject(RecipesService);
 
+  search = signal('');
   selectedCategory = signal<string | null>(null);
   categories = computed(() => this.recipesService.categories() ?? []);
   recipes = computed(() => {
@@ -27,6 +36,7 @@ export class RecipesHome {
     if (!cat) return Promise.resolve([]);
     return this.recipesService.recipesResource(cat)();
   });
+  searchResults = this.recipesService.searchResults;
 
   constructor() {
     this.recipesService.fetchCategories();
@@ -38,5 +48,11 @@ export class RecipesHome {
 
   onRecipeClick(recipe: any) {
     // Naviguer vers la page détail recette si besoin
+  }
+
+  async onSearchInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.search.set(value);
+    await this.recipesService.searchRecipes(value);
   }
 }
