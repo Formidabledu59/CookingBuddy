53271432a634779ce8156fb304456143c201d1c4
Ayoub Fathallah <130578160+Formidabledu59@users.noreply.github.com>
2025-06-13 21:21:42 +0200

    feat: improuve recipe components with category selection

--- src/app/features/recipes/recipe-card/recipe-card.ts
+++ src/app/features/recipes/recipe-card/recipe-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
   templateUrl: './recipe-card.html',
   styleUrl: './recipe-card.scss'
 })
 export class RecipeCard {
-
+  @Input() recipe: any;
+  @Output() recipeClick = new EventEmitter<any>();
 }
--- src/app/features/recipes/recipe-category/recipe-category.ts
+++ src/app/features/recipes/recipe-category/recipe-category.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecipeCard } from '../recipe-card/recipe-card';
 
 @Component({
   selector: 'app-recipe-category',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RecipeCard],
   templateUrl: './recipe-category.html',
   styleUrl: './recipe-category.scss'
 })
 export class RecipeCategory {
-
+  @Input() recipes: any[] = [];
+  @Output() recipeClick = new EventEmitter<any>();
 }
